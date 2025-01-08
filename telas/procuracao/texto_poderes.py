87c35a6f0a075912642c36b3aee1aab08bbe9e62
Nemezs <menezesd288@gmail.com>
2025-01-08 10:02:36 -0300

    correcao de nomenclatura

---
+++ telas/procuracao/texto_poderes.py
@@ -0,0 +1,25 @@
+# textos_poderes.py
+
+# Dicionário contendo os textos predefinidos
+TEXTOS_PODERES = {
+    "Modelo 1 - Queixa-Crime": (
+        "Pelo presente instrumento particular de mandato, confiro amplos poderes ao(s) outorgado(s) acima qualificado(s) "
+        "para, em meu nome, propor, subscrever e acompanhar Queixa-Crime em face de {nome do querelado}, em razão da prática do crime de "
+        "{indicar o crime, por exemplo, calúnia, difamação, injúria, ameaça etc.}, ocorrido em {data ou período aproximado}, na localidade de {informar local}."
+    ),
+    "Modelo 2 - Descrição dos Fatos": (
+        "Os fatos criminosos consistem em [descrever brevemente os fatos: exemplo – 'o querelado proferiu acusações falsas contra o(a) outorgante, "
+        "atribuindo-lhe a prática de fatos sabidamente inverídicos, com o intuito de lesar sua honra e reputação, por meio de mensagens de texto enviadas "
+        "em [data] pelo aplicativo [nome do aplicativo]' ou 'ameaças realizadas em público no dia [data], durante o evento [descrever].']."
+    ),
+    "Modelo 3 - Poderes Especiais": (
+        "Outrossim, fica o(s) procurador(es) autorizado(s) a representar-me em todos os atos necessários ao regular andamento da referida ação penal privada, "
+        "podendo, para tanto, celebrar acordos, transigir, desistir, renunciar, receber e dar quitação, apresentar memoriais e interpor recursos, "
+        "com poderes especiais para a propositura e acompanhamento da queixa-crime."
+    ),
+}
+
+ADVOGADO_OAB = {
+    "MAYKLENE MICHELITT PEREIRA NUNES" :("MAYKLENE MICHELITT PEREIRA NUNES, OAB/TO 12.117-A e OAB/PA 27.056"),
+    "DANIEL MENEZES DE ALMEIDA" :("DANIEL MENEZES DE ALMEIDA, OAB/TO 12.345-A e OAB/GO 54.321")
+}
